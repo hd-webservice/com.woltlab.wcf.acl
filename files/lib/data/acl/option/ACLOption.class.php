5e9b72ad6eb0000050fe51d1dbd274fb7aff84a0
Alexander Ebert <ebert@woltlab.com>
2011-12-28 19:17:40 +0100

    Implemented replacement mechanism for ACL

--- files/lib/data/acl/option/ACLOption.class.php
+++ files/lib/data/acl/option/ACLOption.class.php
@@ -22,4 +22,19 @@ class ACLOption extends DatabaseObject {
 	 * @see	wcf\data\DatabaseObject::$databaseTableIndexName
 	 */
 	protected static $databaseTableIndexName = 'optionID';
+	
+	/**
+	 * Returns a list of options by object type id.
+	 * 
+	 * @param	integer		$objectTypeID
+	 * @return	wcf\data\acl\option\ACLOptionList
+	 */
+	public static function getOptions($objectTypeID) {
+		$optionList = new ACLOptionList();
+		$optionList->getConditionBuilder()->add("acl_option.objectTypeID = ?", array($objectTypeID));
+		$optionList->sqlLimit = 0;
+		$optionList->readObjects();
+		
+		return $optionList;
+	}
 }
--- files/lib/system/acl/ACLHandler.class.php
+++ files/lib/system/acl/ACLHandler.class.php
@@ -1,8 +1,12 @@
 <?php
 namespace wcf\system\acl;
+use wcf\data\acl\option\ACLOption;
+use wcf\data\acl\option\ACLOptionList;
 use wcf\data\object\type\ObjectTypeCache;
+use wcf\system\database\util\PreparedStatementConditionBuilder;
 use wcf\system\exception\SystemException;
 use wcf\system\SingletonFactory;
+use wcf\system\WCF;
 
 class ACLHandler extends SingletonFactory {
 	/**
@@ -31,5 +35,69 @@ class ACLHandler extends SingletonFactory {
 		}
 		
 		return $this->availableObjectTypes[$objectType]->objectTypeID;
+	}
+	
+	/**
+	 * Saves acl for a given object.
+	 * 
+	 * @param	integer		$objectID
+	 * @param	integer		$objectTypeID
+	 */
+	public function save($objectID, $objectTypeID) {
+		// get options
+		$optionList = ACLOption::getOptions($objectTypeID);
+		
+		$this->replaceValues($optionList, 'group', $objectID);
+		$this->replaceValues($optionList, 'user', $objectID);
+	}
+	
+	/**
+	 * Replaces values for given type and object.
+	 * 
+	 * @param	wcf\data\acl\option\ACLOptionList	$optionList
+	 * @param	string					$type
+	 * @param	integer					$objectID
+	 */
+	protected function replaceValues(ACLOptionList $optionList, $type, $objectID) {
+		$options = $optionList->getObjects();
+		
+		// remove previous values
+		$conditions = new PreparedStatementConditionBuilder();
+		$conditions->add("optionID IN (?)", array(array_keys($options)));
+		$conditions->add("objectID = ?", array($objectID));
+		
+		$sql = "DELETE FROM	wcf".WCF_N."_acl_option_to_".$type."
+			".$conditions;
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute($conditions->getParameters());
+		
+		// add new values if given
+		if (!isset($_POST['aclValues']) || !isset($_POST['aclValues'][$type])) {
+			return;
+		}
+		
+		$sql = "INSERT INTO	wcf".WCF_N."_acl_option_to_".$type."
+					(optionID, objectID, ".$type."ID, optionValue)
+			VALUES		(?, ?, ?, ?)";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$values =& $_POST['aclValues'][$type];
+		
+		WCF::getDB()->beginTransaction();
+		foreach ($values as $typeID => $optionData) {
+			foreach ($optionData as $optionID => $optionValue) {
+				// ignore invalid option ids
+				if (!isset($options[$optionID])) {
+					continue;
+				}
+				
+				$statement->execute(array(
+					$optionID,
+					$objectID,
+					$typeID,
+					$optionValue
+				));
+			}
+		}
+		WCF::getDB()->commitTransaction();
 	}
 }
